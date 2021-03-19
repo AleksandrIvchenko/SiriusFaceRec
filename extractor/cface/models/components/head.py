1cec5e08fdcc891d5101b2e2a8390ec13cee27d5
Serge Kim <sergevkim@gmail.com>
2021-03-19 11:34:27 +0300

    arcface head

--- extractor/cface/models/components/head.py
+++ extractor/cface/models/components/head.py
@@ -1,11 +1,66 @@
 from collections import OrderedDict
+import math
 
+from torch import BoolTensor
 from torch.nn import (
     Linear,
     Module,
+    Parameter,
     Sequential,
     Softmax,
 )
+
+
+class ArcFaceLayer(Module):
+    def __init__(
+            self,
+            in_features: int = 512,
+            out_features: int = 10,
+            m: float = 0.5,
+            s: float = 64,
+        ):
+        super().__init__()
+        self.weights = Parameter(torch.FloatTensor(
+            out_features,
+            in_features,
+        ))
+        self.s = s
+        self.cos_m = math.cos(m)
+        self.sin_m = math.sin(m)
+
+    def forward(
+            self,
+            embeddings,
+            labels,
+        ):
+        cos_theta = torch.mm(
+            self.weight,
+            embeddings,
+        ).clamp(-1, 1)
+        sin_theta = torch.sqrt(1 - torch.pow(cos_theta, 2))
+        #cos(theta + m) = cos(theta)cos(m) - sin(theta)sin(m)
+        cos_theta_m = cos_theta * self.cos_m - sin_theta * self.sin_m
+        cos_theta_m = torch.where(
+            condition=cos_theta>0,
+            x=cos_theta_m,
+            y=cos_theta,
+        )
+
+        mask = BoolTensor(cos_theta.shape[0], cos_theta.shape[1])
+        mask.zero_()
+        mask.scatter_(
+            dim=1,
+            index=labels,
+            src=1,
+        )
+        output = torch.where(
+            condition=mask,
+            x=cos_theta_m,
+            y=cos_theta,
+        )
+        output *= self.s
+
+        return output
 
 
 class Head(Module):
@@ -14,22 +69,31 @@ class Head(Module):
             mode: str = 'softmax',
             in_features: int = 512,
             out_features: int = 10,
+            m: float = 0.5,
+            s: float = 64,
         ):
         super().__init__()
+        '''
         head_dict = OrderedDict()
-        head_dict['linear'] = Linear(
-            in_features=in_features,
-            out_features=out_features,
-        )
         if mode == 'softmax':
+            head_dict['linear'] = Linear(
+                in_features=in_features,
+                out_features=out_features,
+            )
             head_dict['last'] = Softmax()
         elif mode == 'arc':
-            head_dict['last'] = Softmax()
-
+            head_dict['last'] = ArcFaceModule()
         self.head = Sequential(head_dict)
+        '''
+        self.cos_m = math.cos(m)
+        self.sin_m = math.sin(m)
 
-    def forward(self, x):
-        return self.head(x)
+    def forward(
+            self,
+            embeddings,
+            labels,
+        ):
+        d
 
 
 if __name__ == '__main__':
