6ac2c16248255fe2fa07152c4dfb457b2dbf55e8
https://github.com/Serega200010/SiriusFM-13 <Serega200010@mail.ru>
2021-03-21 12:47:07 -0400

    feat: базовый модуль inference, база данных sqllite

--- app/main.py
+++ app/main.py
@@ -1,16 +1,19 @@
 import torch
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, Form,  Request, UploadFile
 from fastapi.responses import PlainTextResponse
+from fastapi.templating import Jinja2Templates
 
 from check_inference import check_inference, check_live
 from inference import parse_image
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
 
-@app.get('/')
-def read_root():
-    return 'Hello'
+
+@app.get("/")
+async def root(request: Request, message='Sirius ML Face Recognition'):
+    return templates.TemplateResponse("index.html", {"request": request, "message": message})
 
 
 @app.post('/parse/', response_class=PlainTextResponse)
