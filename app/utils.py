8397a84e9d0d5c089f660e39cbd30e319fb0e642
Serge Kim <sergevkim@gmail.com>
2021-03-22 18:39:54 +0300

    logic

---
+++ app/utils.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+
+
+def extractor_preprocessing(
+        img,
+        ldm,
+        resize: int = 128,
+    ):
+    src = ldm
+    dst = np.array(
+        [
+            [38.2946, 51.6963],
+            [73.5318, 51.5014],
+            [56.0252, 71.7366],
+            [41.5493, 92.3655],
+            [70.7299, 92.2041],
+        ],
+        dtype=np.float32,
+    )
+    dst = dst * resize / 112
+    M, inliers = cv2.estimateAffinePartial2D(
+        src,
+        dst,
+        method=cv2.RANSAC,
+        ransacReprojThreshold=5,
+    )
+    face = cv2.warpAffine(
+        img,
+        M,
+        (resize, resize),
+    )
+
+    return face
